f54edd17fef61ca627abcea8e81c84c1e250be95
Vinod Kumar <vinodkumar@Vinods-MacBook-Air.local>
2022-08-08 19:33:29 +0530

    pending

---
+++ src/test/java/soundsystem/CDPlayerTest2.java
@@ -0,0 +1,40 @@
+package soundsystem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = CDPlayerConfig.class)
+public class CDPlayerTest2 {
+
+	@Autowired
+	private MediaPlayer player;
+	
+	@Autowired
+	private CompactDisc compactDisc;
+	
+	@Rule
+	public final SystemOutRule log = new SystemOutRule().enableLog();
+	
+	@Test
+	public void cdShouldNotBeNull() {
+		assertNotNull(compactDisc);
+	}
+	
+	@Test
+	public void play() {
+		player.play();
+		assertEquals("Playing Sgt. Pepper's Lonely Hearts Club Band" +
+				" by The Beatles\n",
+				log.getLog());
+	}
+	
+}
